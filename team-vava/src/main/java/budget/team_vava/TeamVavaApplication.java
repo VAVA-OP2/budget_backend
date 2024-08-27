e018a4e46d67e59ef23c6dacee91fbde40a01802
anastasialamberg <anastasia.lamberg@gmail.com>
2024-08-27 18:47:56 +0300

    Created Spring Boot project base

---
+++ team-vava/src/main/java/budget/team_vava/TeamVavaApplication.java
@@ -0,0 +1,13 @@
+package budget.team_vava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamVavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamVavaApplication.class, args);
+	}
+
+}
---
+++ team-vava/src/test/java/Budget/team_vava/TeamVavaApplicationTests.java
@@ -0,0 +1,13 @@
+package Budget.team_vava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamVavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
